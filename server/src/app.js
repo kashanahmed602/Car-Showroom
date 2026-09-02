86e3c9160f8bf294468c32146e3a784b7a986754
Kashan Ahmed <kashanahmed3105@gmail.com>
2026-09-02 21:14:58 +0500

    deploying 3

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.VITE_API_URL || 'http://localhost:5173',
     credentials: true
 }));
 
